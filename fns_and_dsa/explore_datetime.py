1d789ab116595b924c6dd859f733a929684819f7
Prosper Chigozie <prospachigoz@gmail.com>
2025-01-13 02:45:03 +0100

    Update explore_datetime.py

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -1,7 +1,7 @@
 from datetime
 
 def display_current_datetime():
-   current_date = datetime.date(2025,1,12)
+   current_date = datetime.datetime.now()
    print("Current date and time: ", current_date.strftime("%Y-%m-%d %H:%M:%S"))
    
 display_current_datetime()
