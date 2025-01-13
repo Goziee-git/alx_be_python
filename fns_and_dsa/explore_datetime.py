7ed5f1ccd537095cab92c9bf97f6bb4f6b0a717f
Prosper Chigozie <prospachigoz@gmail.com>
2025-01-13 02:48:32 +0100

    Update explore_datetime.py

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -8,8 +8,8 @@ display_current_datetime()
 
 def calculate_future_date():
    number_of_days = int(input("Enter the number of days to add to the current date: "))
-   future_date = datetime.date(2025,1,12) + datetime.timedelta(days=number_of_days)
-   print(f"future date: {future_date}")
+   future_date = datetime.datetime.now() + datetime.timedelta(days=number_of_days)
+   print(f"future date: {future_date.strftime("%Y-%m-%d")}")
 
 calculate_future_date()
 
