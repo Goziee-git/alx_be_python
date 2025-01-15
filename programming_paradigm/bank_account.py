851502f19d70e1e25334bde133dc2279866c15c8
Goziee-git <prospachigoz@gmail.com>
2025-01-15 16:31:44 +0100

    added bank_account.py

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -7,11 +7,11 @@ class BankAccount:
       self.account_balance += amount
 
    def withdraw(self, amount):
-      if self.account_balance >= amount:
-         self.account_balance -= amount
-         return True
-      else:
-         return False
+        if self.account_balance >= amount:
+            self.account_balance -= amount
+            return True
+        else:
+            return False
 
-   def display_balance(self):
-      print(f"Current balance: ${self.account_balance:.2f}")
+    def display_balance(self):
+        print(f"Current balance: ${self.account_balance:.2f}")
