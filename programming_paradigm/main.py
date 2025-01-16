135030e3d34dbe1b46387d892fc01bd4caa9a4e3
Prosper Chigozie <prospachigoz@gmail.com>
2025-01-16 14:50:43 +0100

    Update main.py

--- programming_paradigm/main.py
+++ programming_paradigm/main.py
@@ -5,12 +5,13 @@ def main():
     if len(sys.argv) != 3:
         print("Usage: python main.py <numerator> <denominator>")
         sys.exit(1)
+    try:
+        numerator = float(sys.argv[1])
+        denominator = float(sys.argv[2])
+        result = safe_divide(numerator, denominator)
+        print(f"The result of the division is {result}")
 
-    numerator = sys.argv[1]
-    denominator = sys.argv[2]
-
-    result = safe_divide(numerator, denominator)
-    print(f"The result of the division is {result}")
-
+    except ValueError as e:
+        print(f"Error: {e}", file=sys.stderr)
 if __name__ == "__main__":
     main()
