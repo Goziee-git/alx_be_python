965f727691ad421d8c6497a4bc9338adeb81e196
Goziee-git <prospachigoz@gmail.com>
2025-01-19 12:00:05 +0100

    new files for week6 exercises

--- programming_paradigm/main.py
+++ programming_paradigm/main.py
@@ -5,12 +5,15 @@ def main():
     if len(sys.argv) != 3:
         print("Usage: python main.py <numerator> <denominator>")
         sys.exit(1)
+    try:
+        numerator = float(sys.argv[1])
+        denominator = float(sys.argv[2])
+        result = safe_divide(numerator, denominator)
+        print(f"The result of the division is {result}")
 
-    numerator = sys.argv[1]
-    denominator = sys.argv[2]
+    except ValueError as e:
+        print(f"Error: {e}", file=sys.stderr)
 
-    result = safe_divide(numerator, denominator)
-    print(f"The result of the division is {result}")
-
+        
 if __name__ == "__main__":
     main()
